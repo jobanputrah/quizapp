a60cbe077151388f8e08279d9b2534c04f24bb30
Harsh Jobanputra <hjobanpu@stevens.edu>
2020-04-16 01:09:05 -0400

    Better cors handling using 'cors' middleware

--- server/server.js
+++ server/server.js
@@ -1,14 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const api = require('./api');
 const app = express();
 
+app.use(cors());
 app.use(bodyParser.json());
-app.use((_, res, next) => {
-    // TODO: make this a config
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    next();
-});
 app.use('/api', api)
 
 app.use('*', (req, res) => {
@@ -20,4 +17,3 @@ app.use('*', (req, res) => {
 app.listen(8000, () => {
     console.log('Server started at http://localhost:8000/');
 });
-
