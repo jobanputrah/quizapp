35cda304901e7d9f2d2b3de049673d67d26213ae
Harsh Jobanputra <hjobanpu@stevens.edu>
2020-04-18 03:04:09 -0400

    Move rate limit setting to config

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const app = express();
 
 const limiter = rateLimit({
     windowMs: 60 * 60 * 1000, // 1 hour
-    max: 5,
+    max: config.requestLimitPerHr || 50,
     message: 'You have exceeded the 50 requests per hour limit!',
     headers: true,
 });
